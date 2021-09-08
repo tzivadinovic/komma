fbffe54642564fc156167514458cd5534cb589ed
tzivadinovic <zivadinovictomislav@gmail.com>
2021-09-08 20:12:50 +0200

    added roles page, and role controller with getMapping

---
+++ src/main/java/com/tzivadinovic/komma/controller/RoleController.java
@@ -0,0 +1,20 @@
+package com.tzivadinovic.komma.controller;
+
+import com.tzivadinovic.komma.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class RoleController {
+    private final RoleService roleService;
+
+    @GetMapping
+    public String getRoles(Model model) {
+        model.addAttribute("roles", roleService.findAll());
+        return "roles/roles";
+    }
+
+}
