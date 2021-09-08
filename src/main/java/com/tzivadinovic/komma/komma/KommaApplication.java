26e9fb9163f8c3ec93d13c20110debef07109201
tzivadinovic <zivadinovictomislav@gmail.com>
2021-09-08 18:13:29 +0200

    initial commit

---
+++ src/main/java/com/tzivadinovic/komma/komma/KommaApplication.java
@@ -0,0 +1,13 @@
+package com.tzivadinovic.komma.komma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tzivadinovic/komma/komma/KommaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzivadinovic.komma.komma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
